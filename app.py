c19d4bd9ba3c51abc7fddcc8441781fe0389abe4
uglyz <uglyzxenun@gmail.com>
2023-08-31 21:51:35 +0300

    initial commit

--- app.py
+++ app.py
@@ -1,15 +1,86 @@
-# write a program that user need to guess a secret number. first ask the user to guess the number. guess can be maximum be three times. if
-# guess is correct within three guess time, user win otherwise failed.
-secret_number = 9
-guess_count = 0
-guess_limit = 3
+"""
+question 1
+name = input("Enter your name: ")
+print(f"Hello" ,name)
+"""
 
-while guess_count < guess_limit:
-    guess = int(input("enter your guess: "))
-    guess_count +=1
-    if guess == secret_number:
-        print("You win!")
-        break
+""" question 2
+radius = float(input("Enter the radius of circle: "))
+pie = 3.14
+area = 2*radius*pie*pie
+print(f"The area of the circle is: {area} ")
+"""
+""" question 3
+length = float(input("Enter length of rectangle: "))
+width = float(input("Enter width of rectangle: "))
+perimeter = 2*length + 2*width
+area = length * width
+print(f"The perimeter of the rectangle is: {perimeter}")
+print(f"The area of the rectangle is: {area}")
+"""
+"""
+a = int(input("Enter a first number: "))
+b = int(input("Enter a second number: "))
+c = int(input("Enter a third number: "))
+d = a+b+c
+e = a*b*c
+f = d/3
+print(f"Sum of the numbers is: {d}")
+print(f"Product of the numbers is: {e}")
+print(f"Average of the numbers is: {f}")
 
+print(f"Sum of the numbers is", a+b+c)
+print(f"Product of the numbers is", a*b*c)
+print(f"Average of the numbers is", (a+b+c)/3)
+"""
+
+""" question 5
+talents = float(input("Enter talents: "))
+pounds = float(input("Enter pounds: "))
+lots = float(input("Enter lots: "))
+pounds1 = pounds + (20*talents)
+lots1 = lots + (32*pounds1)
+grams = lots1 * 13.3
+kilograms = grams // 1000
+remaining_grams = grams % 1000
+print(f"the total weights in modern units is: {kilograms:.0f} kilogrgams and {remaining_grams:.2f} grams. ")
+"""
+
+
+
+""" module 3 question 1
+zander = float(input("Tell the lenght of zander: "))
+zander_less = 42 - zander
+if(zander < 42):
+    print("Release the fish into the lake")
+    print(f"You catch {zander_less:0.2f} cm below the limit. ")
 else:
-    print("Sorry, You Failed!")
+    print("Put the fish inside the jar.")
+"""
+""" question 2
+cabin_class = input("Enter your cabin class:  ")
+cabin_class_upper = cabin_class.upper()
+if(cabin_class_upper == "LUX"):
+    print("Your cabin is upper-deck cabin with balcony.")
+elif(cabin_class_upper == "A"):
+    print("Your cabin is above the car deck, equipped with a window.")
+elif(cabin_class_upper == "B"):
+    print("Your cabin is windowless cabin above the car deck.")
+elif(cabin_class_upper == "C"):
+    print("Your cabin is windowless below the car deck.")
+else:
+    print("Invalid cabin class") """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
