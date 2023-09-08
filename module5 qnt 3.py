88b7f9fdbae2731a3e31fef4574f8870c52ecaed
uglyz <uglyzxenun@gmail.com>
2023-09-08 23:35:58 +0300

    initial commit

---
+++ module5 qnt 3.py
@@ -0,0 +1,14 @@
+user = input("Enter your number: ")
+user = int(user)
+if user == 1:
+    print(user,"number is not a prime number. ")
+elif user > 1:
+   for i in range(2,user):
+     if(user % i == 0):
+      print(user, "is not prime number")
+      break
+   else:
+     print(user,"is a prime number.")
+
+else:
+  print(user,"is not a prime number")
