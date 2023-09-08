47ec3b35b3c0f4613f9f35665e050ab55e8b4bf0
uglyz <uglyzxenun@gmail.com>
2023-09-08 10:32:59 +0300

    initial commit

---
+++ soar.py
@@ -0,0 +1,8 @@
+user = ["Viivi", "Ahmed", "Pekka", "Olga", "Mary"]
+user.append("hello")
+print(user)
+print(user[-3:-1])
+print(user.index("Ahmed"))
+loser = ["roar", "soar"]
+user.extend(loser)
+print(user)
